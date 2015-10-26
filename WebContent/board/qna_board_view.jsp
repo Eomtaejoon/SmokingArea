7823c6f41c24bfbe8402d0a339c97807b13cde0d
wonjunsik <wpfk1@naver.com>
2015-10-26 15:15:55 +0900

    게시판 MVC 업데이트 수정중

---
+++ WebContent/board/qna_board_view.jsp
@@ -0,0 +1,97 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"%>
+<%@ page import="net.board.db.*" %>
+<%
+	BoardBean board = (BoardBean)request.getAttribute("boarddata");
+	System.out.print(board.getBOARD_CHAT());
+%>
+
+<html>
+<head>
+	<title>MVC 게시판</title>
+</head>
+
+<body>
+<!-- 게시판 수정 -->
+<table cellpadding="0" cellspacing="0">
+	<tr align="center" valign="middle">
+		<td colspan="5">MVC 게시판</td>
+	</tr>
+	
+	<tr>
+		<td style="font-family:돋음; font-size:12" height="16">
+			<div align="center">제 목&nbsp;&nbsp;</div>
+		</td>
+		
+		<td style="font-family:돋음; font-size:12">
+		<%="<"%><%=board.getBOARD_YASIK() %><%=">"%><%=board.getBOARD_SUBJECT()%>
+		</td>
+	</tr>
+	
+	<tr bgcolor="cccccc">
+		<td colspan="3" style="height:1px;">
+		</td>
+	</tr>
+	
+	<tr>
+		<td style="font-family:돋음; font-size:12">
+			<div align="center">내 용</div>
+		</td>
+		
+		<td style="font-family:돋음; font-size:12">
+			<table border=0 width=200 height=100 style="table-layout:fixed">
+				<tr>
+					<td valign=top style="font-family:돋음; font-size:12">
+					<%=board.getBOARD_CONTENT() %>
+					</td>
+				</tr>
+			</table>
+		</td>
+		
+		<td>
+			<table border=0 width=200 height=100>
+				<tr>
+					<td>
+					<embed height="100%" width="100%" src="http://www.gagalive.kr/livechat1.swf?chatroom=~~~<%=board.getBOARD_CHAT()%>"></embed>
+					</td>
+				</tr>
+			</table>
+		</td>		
+	</tr>
+	<tr>
+		<td style="font-family:돋음; font-size:12">
+			<div align="center">첨부파일</div>
+		</td>
+		
+		<td style="font-family:돋음; font-size:12">
+		<%if(!(board.getBOARD_FILE()==null)){ %>
+		<a href="c:\\upload\\<%=board.getBOARD_FILE()%>">
+			<%=board.getBOARD_FILE() %>
+		</a>
+		<%} %>
+		</td>
+	</tr>
+	<tr bgcolor="cccccc">
+		<td colspan="3" style="height:1px;"></td>
+	</tr>
+	<tr><td colspan="2">&nbsp;</td></tr>
+	<tr align="center" valign="middle">
+		<td colspan="5">
+			<font size=2>
+			<a href="./BoardReplyAction.bo?num=<%=board.getBOARD_NUM() %>">
+			[답변]
+			</a>&nbsp;&nbsp;
+			<a href="./BoardModify.bo?num=<%=board.getBOARD_NUM() %>">
+			[수정]
+			</a>&nbsp;&nbsp;
+			<a href="./BoardDeleteAction.bo?num=<%=board.getBOARD_NUM() %>"
+			>
+			[삭제]
+			</a>&nbsp;&nbsp;
+			<a href="./BoardList.bo">[목록]</a>&nbsp;&nbsp;
+			</font>
+		</td>
+	</tr>
+</table>
+<!-- 게시판 수정 -->
+</body>
+</html>
---
+++ src/net/board/action/BoardFrontController.java
@@ -0,0 +1,102 @@
+package net.board.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+ public class BoardFrontController 
+ 	extends javax.servlet.http.HttpServlet 
+ 	implements javax.servlet.Servlet {
+	 protected void doProcess(HttpServletRequest request, HttpServletResponse response) 
+	 	throws ServletException, IOException {
+		 String RequestURI=request.getRequestURI();
+		 String contextPath=request.getContextPath();
+		 String command=RequestURI.substring(contextPath.length());
+		 ActionForward forward=null;
+		 Action action=null;
+		  
+		 System.out.println(command);
+		 
+		   if(command.equals("/member/BoardWrite.bo")){
+			   forward=new ActionForward();
+			   forward.setRedirect(false);
+			   forward.setPath("../board/qna_board_write.jsp");
+		   }else if(command.equals("/member/BoardReplyAction.bo")){
+			   action = new BoardReplyView();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+		   }else if(command.equals("/member/BoardModify.bo")){
+			   action = new BoardModifyView();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+	 	   }else if(command.equals("/member/BoardAddAction.bo")){
+			   action  = new BoardAddAction();
+			   try {
+				   forward=action.execute(request, response );
+			   } catch (Exception e) {
+				   e.printStackTrace();
+			   }
+		   }else if(command.equals("/member/BoardReplyView.bo")){
+			   action = new BoardReplyAction();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+		   }else if(command.equals("/member/BoardModifyAction.bo")){
+			   action = new BoardModifyAction();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+		   }else if(command.equals("/member/BoardDeleteAction.bo")){
+			   action = new BoardDeleteAction();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+		   }else if(command.equals("/member/BoardList.bo")){
+			   action = new BoardListAction();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+		   }else if(command.equals("/member/BoardDetailAction.bo")){
+			   action = new BoardDetailAction();
+			   try{
+				   forward=action.execute(request, response);
+			   }catch(Exception e){
+				   e.printStackTrace();
+			   }
+		   }
+		   
+		   if(forward.isRedirect()){
+			   response.sendRedirect(forward.getPath());
+		   }else{
+			   RequestDispatcher dispatcher=
+				   request.getRequestDispatcher(forward.getPath());
+			   dispatcher.forward(request, response);
+		   }
+	 }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		doProcess(request,response);
+	}  	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		doProcess(request,response);
+	}   	  	    
+}
